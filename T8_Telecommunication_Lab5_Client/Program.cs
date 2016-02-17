f576300c4e5959d31eb615d8761a8e2fa8a2ee79
Max Bondarchuk <bondarchuk.m.y@gmail.com>
2016-02-17 22:13:26 +0200

    Initial

---
+++ T8_Telecommunication_Lab5_Client/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T8_Telecommunication_Lab5_Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
